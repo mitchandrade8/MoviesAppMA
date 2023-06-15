ca9191279caafe3a889976714b8eb27c6b2c1152
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 11:15:55 -0600

    -- Start - Delete a movie

---
+++ MoviesAppMA/Views/MovieListView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListView.swift
+//  MoviesAppMA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListView()
+}
