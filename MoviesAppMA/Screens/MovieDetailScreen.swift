b8d1d045292303f56e8d64938b75d03a4f6785ed
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 15:14:27 -0600

    -- Start - updating a movie

---
+++ MoviesAppMA/Screens/MovieDetailScreen.swift
@@ -0,0 +1,64 @@
+//
+//  MovieDetailScreen.swift
+//  MoviesAppMA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MovieDetailScreen: View {
+    
+    @Environment(\.modelContext) private var context
+    let movie: Movie
+    
+    @State private var title: String = ""
+    @State private var year: Int?
+    
+    var body: some View {
+        Form {
+            TextField("Title", text: $title)
+            TextField("Year", value: $year, format: .number)
+            Button("Update") {
+                guard let year = year else { return }
+                
+                movie.title = title
+                movie.year = year
+                
+                do {
+                    try context.save()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        .onAppear {
+            title = movie.title
+            year = movie.year
+        }
+    }
+}
+
+struct MovieDetailContainerScreen: View {
+    
+    @Environment(\.modelContext) private var context
+    @State private var movie: Movie?
+    
+    var body: some View {
+        ZStack {
+            if let movie {
+                MovieDetailScreen(movie: movie)
+            }
+        }
+            .onAppear {
+                movie = Movie(title: "Spiderman", year: 2023)
+                context.insert(movie!)
+            }
+    }
+}
+
+#Preview {
+    MovieDetailContainerScreen()
+        .modelContainer(for: [Movie.self])
+}
