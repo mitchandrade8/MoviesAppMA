cc6f0698923494a962c6fac3454d57b40b11f356
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 10:21:36 -0600

    -- Start - Fetching and Displaying Movies with SwiftData

---
+++ MoviesAppMA/Screens/MovieListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListScreen.swift
+//  MoviesAppMA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import SwiftUI
+
+struct MovieListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieListScreen()
+}
