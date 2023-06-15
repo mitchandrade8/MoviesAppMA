bd8de06bf5f5337c18c040ff17781a296ddbf4a9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 10:14:35 -0600

    -- Finished - Saving a movie with SwiftData

---
+++ MoviesAppMA/Extensions/String+Ext.swift
@@ -0,0 +1,8 @@
+//
+//  String+Ext.swift
+//  MoviesAppMA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import Foundation
---
+++ MoviesAppMA/Screens/AddMovieScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AddMovieScreen.swift
+//  MoviesAppMA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import SwiftUI
+
+struct AddMovieScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMovieScreen()
+}
