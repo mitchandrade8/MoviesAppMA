41364fb19fd60cff3a6f6fef5a0bb4183d080af1
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-15 15:40:18 -0600

    -- Start - Saving a review to a movie

---
+++ MoviesAppMA/Screens/AddReviewScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AddReviewScreen.swift
+//  MoviesAppMA
+//
+//  Created by Mitch Andrade on 6/15/23.
+//
+
+import SwiftUI
+
+struct AddReviewScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddReviewScreen()
+}
